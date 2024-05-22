689c139128c77cd971a96aa51d277e9f0c942784
ToxicShado <toxicshadowns@gmail.com>
2024-05-22 20:18:33 +0200

    +lista prijatelja (basic verzija), small random changes

--- SocijalnaMreza/Korisnik.cs
+++ SocijalnaMreza/Korisnik.cs
@@ -16,6 +16,9 @@ namespace SocijalnaMreza
         private DateOnly datumRodjenja;
         private Image profilnaSlika;                            //treba li getter i setter za ovo
         private ObservableCollection<Post> objavljeniPostovi;
+        private ObservableCollection<Korisnik> listaPrijatelja;
+
+
 
         public Korisnik(string id, string ime, string prezime, DateOnly datumRodjenja, Image profilnaSlika)
         {
@@ -25,7 +28,10 @@ namespace SocijalnaMreza
             this.datumRodjenja = datumRodjenja;
             this.profilnaSlika = profilnaSlika;
             objavljeniPostovi = new ObservableCollection<Post>();
+            listaPrijatelja = new ObservableCollection<Korisnik>();
         }
+
+
 
         /*
          private string id;
@@ -47,10 +53,63 @@ namespace SocijalnaMreza
 
 
 
+        
+
+
+        public bool DodajPrijatelja(Korisnik k)
+        {
+            if (k == null || listaPrijatelja.Contains(k)) return false;
+            listaPrijatelja.Add(k);
+            return true;
+        }
+
+        public bool ukloniPrijatelja(Korisnik k)
+        {
+            return listaPrijatelja.Remove(k);
+        }
+
+        public ObservableCollection<Korisnik> getFriends()
+        {
+            return listaPrijatelja;
+        }
+
+        /*
+        //ovako bi izgledalo da zelimo listu sa stringovima (id-evima) prijatelja
+        private ObservableCollection<string> listaPrijatelja;
+
+        public bool DodajPrijatelja(string s)
+        {
+            if (s == null || s.Length == 0 || listaPrijatelja.Contains(s)) return false;
+            listaPrijatelja.Add(s);
+            return true;
+        }
+
+        public bool DodajPrijatelja(Korisnik k)
+        {
+            if (k == null || listaPrijatelja.Contains(k.Id)) return false;
+            listaPrijatelja.Add(k.Id);
+            return true;
+        }
+
+
+        public bool ukloniPrijatelja(string s)
+        {
+            return listaPrijatelja.Remove(s);
+        }
+        public bool ukloniPrijatelja(Korisnik k)
+        {
+            return listaPrijatelja.Remove(k.Id);
+        }
+        */
 
 
 
 
+
+        public override string ToString()
+        {
+            return "Ime : " + ime + ", Prezime :" + prezime;
+        }
 
 
         public string Id
--- SocijalnaMreza/MainWindow.xaml.cs
+++ SocijalnaMreza/MainWindow.xaml.cs
@@ -18,6 +18,9 @@ namespace SocijalnaMreza
     {
         static Random idGen = new Random();
         Korisnik glavniKorisnik = new Korisnik(idGen.Next().ToString(), "Nemanja", "Vojnov", DateOnly.FromDateTime(DateTime.Now), null);
+        Korisnik drugiKorisnik = new Korisnik(idGen.Next().ToString(),"Nikola", "Kovac",DateOnly.FromDateTime(DateTime.Now),null);
+        Korisnik treci = new Korisnik(idGen.Next().ToString(),"random1", "kk",DateOnly.FromDateTime(DateTime.Now),null);
+        Korisnik cetvrti = new Korisnik(idGen.Next().ToString(),"random2", "lol",DateOnly.FromDateTime(DateTime.Now),null);
 
         public MainWindow()
         {
@@ -27,7 +30,14 @@ namespace SocijalnaMreza
             glavniKorisnik.dodajPost("cao svima");
             glavniKorisnik.dodajPost("cao svima");
             glavniKorisnik.dodajPost("cao svima");
+            
+            glavniKorisnik.DodajPrijatelja(drugiKorisnik);
+            glavniKorisnik.DodajPrijatelja(treci);
+            glavniKorisnik.DodajPrijatelja(cetvrti);
+
             ViewPostsGrid.ItemsSource = glavniKorisnik.getPosts();
+            SviPrijatelji.ItemsSource = glavniKorisnik.getFriends();
+            
         }
 
         private void Toggle_Upload_Visibility_Click(object sender, RoutedEventArgs e)
@@ -43,6 +53,7 @@ namespace SocijalnaMreza
         private void Upload_Post_Click(object sender, RoutedEventArgs e)
         {
             glavniKorisnik.dodajPost(Upload_Post_Content.Text);
+            Upload_Post_Content.Text = "";
         }
     }
 }
